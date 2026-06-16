558abf54bd042440defd761245fcf121a8b594fa
felipebretas15-spec <felipebretas15@gmail.com>
2026-06-16 16:20:22 -0300

    remove all files

--- bbshop/netlify/functions/_db.js
+++
@@ -1,7 +0,0 @@
-const { neon } = require('@neondatabase/serverless');
-
-const CONNECTION = 'postgresql://neondb_owner:npg_tpQ0cBXY3FxR@ep-orange-poetry-ac1qx91b-pooler.sa-east-1.aws.neon.tech/bbshop?sslmode=require';
-
-const sql = neon(CONNECTION);
-
-module.exports = sql;
